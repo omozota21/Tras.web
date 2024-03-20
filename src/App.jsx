4f57d2c154d42f1ad990a18d6fda5231ba762e43
omozota21 <omozota21@iesrioarba.es>
2024-03-20 13:46:01 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Icon from '@mui/material/Icon';
 import AccessAlarmsIcon from '@mui/icons-material/AccessAlarms';
-import { PrimerComponente } from './Componentes/PrimerComponente';
-import { SegundoComponente } from './Componentes/SegundoComponente';
-
+import ResponsiveAppBar from "./Componentes/ResponsiveAppBar.jsx"
+import QuiltedImageList from "./Componentes/Galeria.jsx"
+import Galeria from './Componentes/Galeria.jsx';
 
 
 function App() {
 
 
   return (
-    <>
-      <div>
-          <img></img>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim natus non fuga ratione nobis expedita dolorem alias tempora, nemo dignissimos exercitationem quo saepe? Itaque soluta dolor, exercitationem eligendi repudiandae reprehenderit?</p>
-          <PrimerComponente></PrimerComponente>
-          <SegundoComponente></SegundoComponente>
-      </div>
-
+    <>  
+      <ResponsiveAppBar></ResponsiveAppBar>
+   
+  
+        
     </>
   )
 
--- src/Componentes/SegundoComponente.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const SegundoComponente = () => {
-  return (
-    <div>Adios</div>
-  )
-}
